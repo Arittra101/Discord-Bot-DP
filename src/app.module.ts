a178ff9f6f8df6bb0e3e3359085b22b525bc4afc
Arittra101 <avishek51775@gmail.com>
2026-04-18 04:10:09 +0600

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DiscordModule } from './discord/discord.module';
+import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
+import { ReminderModule } from './reminder/reminder.module';
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: 'src/.env',
+    }),
+    ScheduleModule.forRoot(), 
+
+    DiscordModule,
+    ReminderModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService], 
+})
+export class AppModule { }
---
+++ src/discord/discord.service.ts
@@ -0,0 +1,57 @@
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { Client, GatewayIntentBits, TextChannel } from 'discord.js';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class DiscordService implements OnModuleInit {
+    private readonly logger = new Logger(DiscordService.name);
+    private client: Client;
+
+    constructor(private configService: ConfigService) {
+        this.client = new Client({ intents: [GatewayIntentBits.Guilds] });
+    }
+
+    async onModuleInit() {
+        const token = this.configService.get<string>('DISCORD_BOT_TOKEN');
+        if (!token) {
+            throw new Error(
+                'Missing DISCORD_BOT_TOKEN. Ensure it is set in environment variables or in src/.env.',
+            );
+        }
+        await this.client.login(token);
+
+
+        this.client.once('ready', () => {
+            this.logger.log(`Logged in as ${this.client.user?.tag}!`);
+        });
+    }
+
+
+    async sendMessage(channelId: string, message: string): Promise<void> {
+        try {
+            const channel = await this.client.channels.fetch(channelId);
+            if (!channel || !(channel instanceof TextChannel)) {
+                this.logger.error(`Channel ${channelId} not found or is not a text channel`);
+                return;
+            }
+
+          //  await channel.send(message,);
+            await channel.send({
+                content: message,
+                allowedMentions: {
+                    parse: ['everyone'], 
+                },
+            });
+
+            this.logger.log(`Reminder sent to channel ${channelId}`);
+        } catch (error) {
+            this.logger.error('Failed to send message', error);
+        }
+    }
+}
+
+// exicution order:
+// 1. Property initialization (logger)
+// 2. constructor()
+// 3. onModuleInit()
+// readonly list: ReadonlyArray<string> = ["kdf", "fdf"] like val list = listOf("kdf","fdf")
